3d8118237f137bd9e782a97a49bc7275db86a001
Samuel Martineau <martineau.samuel.1990@gmail.com>
2016-04-27 23:24:17 +0200

    add upload picture config

--- config/index.js
+++ config/index.js
@@ -3,5 +3,13 @@ module.exports = {
     PORT: 3000,
     URL: 'localhost',
     LOGGER_FILE_PATH: 'all-logs.log',
-    API_BASE_URL: '/api'
+    API_BASE_URL: '/api',
+    UPLOADS: {
+    TMP_DIRECTORY: 'temp_uploads/',
+    FILE_NAME: 'winePicture',
+    THUMBNAIL: {
+        WIDTH: 100,
+        HEIGHT: 200,
+        QUALITY: 30
+    }
 };
