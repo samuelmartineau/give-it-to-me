3eb74a0533844053283ce3848c4978b416efd879
Samuel Martineau <martineau.samuel.1990@gmail.com>
2016-04-27 23:24:44 +0200

    add upload action

--- src/common/actions/index.js
+++ src/common/actions/index.js
@@ -1,6 +1,33 @@
+import fetch from 'isomorphic-fetch';
+
+import config from '../../../config';
+import routes from '../../server/routes';
+
 export function setState(state) {
   return {
     type: 'SET_STATE',
     state
   };
+}
+
+function uploadPicture() {
+    return {
+        type: types.UPLOAD_PICTURE
+    };
+}
+
+export function uploadWinePicture(picture) {
+    return dispatch => {
+        const data = new FormData();
+        data.append(config.UPLOADS.FILE_NAME, picture);
+        dispatch(uploadPicture());
+        return fetch(config.API_BASE_URL + routes.PICTURE, {
+            method: 'post',
+            body: data
+        })
+            .catch(error => {
+                debugger
+                throw error;
+            });
+    };
 }
