d0c9d867bb8db60e4d6f64ce0274a1e897903546
Samuel Martineau <martineau.samuel.1990@gmail.com>
2016-04-28 23:37:20 +0200

    promisify + service gm

--- src/server/routes/picture.js
+++ src/server/routes/picture.js
@@ -1,35 +1,30 @@
 import multer from 'multer';
-import {subClass} from 'gm';
 
 import logger from '../utils/logger';
 import config from '../../../config';
 import * as serverConstants from '../../common/constants/server';
+import {generateThumbnail, generateBlur} from '../services/picture';
 
-const gm = subClass({imageMagick: true});
 const upload = multer({ dest: config.UPLOADS_TMP_DIRECTORY });
 
 module.exports = function(router) {
 
     router.post(serverConstants.ROUTES.PICTURE, upload.single(serverConstants.PICTURE_UPLOAD.FILE_NAME), (req, res) => {
-
-        gm(req.file.path)
-            .resize(serverConstants.PICTURE_UPLOAD.THUMBNAIL.WIDTH, serverConstants.PICTURE_UPLOAD.THUMBNAIL.HEIGHT, '^')
-            .noProfile()
-            .gravity('Center')
-            .crop(serverConstants.PICTURE_UPLOAD.THUMBNAIL.WIDTH, serverConstants.PICTURE_UPLOAD.THUMBNAIL.HEIGHT)
-            .quality(serverConstants.PICTURE_UPLOAD.THUMBNAIL.QUALITY)
-            .write(config.UPLOADS_TMP_DIRECTORY + 'test.png', (err) => {
-                if (err) {
-                    logger.debug('gm service error, gm resize', err);
-                }
-
-                gm(config.UPLOADS_TMP_DIRECTORY + 'test.png')
-                    .resize(2, 3)
-                    .toBuffer('GIF', function (error, buffer) {
-                        logger.info('data:image/gif;base64,' + buffer.toString('base64'));
-                    });
+        let thumbnailPath;
+        generateThumbnail(req.file.path)
+            .then(tempFilePath => {
+                thumbnailPath = tempFilePath;
+                return generateBlur(tempFilePath);
+            })
+            .then(blur => {
+                res.json({
+                    tempFilePath: thumbnailPath,
+                    blur: blur
+                });
+            })
+            .catch(error => {
+                logger.error('error during picture processing', error);
+                res.status(500).json({error: error});
             });
-
-        res.json('good');
     });
 };
---
+++ src/server/services/picture.js
@@ -0,0 +1,43 @@
+import {subClass} from 'gm';
+import uuid from 'node-uuid';
+import path from 'path';
+
+import config from '../../../config';
+import * as serverConstants from '../../common/constants/server';
+
+const gm = subClass({imageMagick: true});
+
+export function generateThumbnail(sourcePath) {
+    return new Promise(
+        function(resolve, reject) {
+            var tmpFileName = path.join(config.UPLOADS_TMP_DIRECTORY, uuid.v1());
+            gm(sourcePath)
+                .resize(serverConstants.PICTURE_UPLOAD.THUMBNAIL.WIDTH, serverConstants.PICTURE_UPLOAD.THUMBNAIL.HEIGHT, '^')
+                .noProfile()
+                .gravity('Center')
+                .crop(serverConstants.PICTURE_UPLOAD.THUMBNAIL.WIDTH, serverConstants.PICTURE_UPLOAD.THUMBNAIL.HEIGHT)
+                .quality(serverConstants.PICTURE_UPLOAD.THUMBNAIL.QUALITY)
+                .write(tmpFileName, (error) => {
+                    if (error) {
+                        return reject(error);
+                    }
+
+                    return resolve(tmpFileName);
+                });
+        });
+}
+
+export function generateBlur(path) {
+    return new Promise(
+        function(resolve, reject) {
+            gm(path)
+                .resize(2, 3)
+                .toBuffer('GIF', (error, buffer) => {
+                    if (error) {
+                        return reject(error);
+                    }
+
+                    return resolve(`data:image/gif;base64,${buffer.toString('base64')}`);
+                });
+        });
+}
