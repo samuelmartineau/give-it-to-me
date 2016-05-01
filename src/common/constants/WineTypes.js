6382a65a4d24e443a8b7ae9afde0f84f57dea290
Samuel Martineau <martineau.samuel.1990@gmail.com>
2016-05-01 22:53:04 +0200

    domain constants

---
+++ src/common/constants/WineTypes.js
@@ -0,0 +1,35 @@
+export const WINE_TYPES = {
+  RED: {
+    label: 'Rouge',
+    types: ['REGULAR', 'SPARKLING', 'DRY']
+  },
+  WHITE: {
+    label: 'Blanc',
+    types: ['REGULAR', 'SPARKLING', 'LIQUOROUS', 'DRY']
+  },
+  CHAMPAGNE: {
+    label: 'Champagne',
+    types: ['REGULAR', 'PINK']
+  }
+};
+
+export const WINE_CATEGORIES = {
+  REGULAR: {
+    label: ''
+  },
+  SPARKLING: {
+    label: 'effervescent'
+  },
+  LIQUOROUS: {
+    label: 'liquoreux'
+  },
+  SOFT: {
+    label: 'moelleux'
+  },
+  DRY: {
+    label: 'sec'
+  },
+  PINK: {
+    label: 'rosé'
+  }
+};
