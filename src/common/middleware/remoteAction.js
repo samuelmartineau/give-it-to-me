faf0d9a1fd05f6003b08734dcf92c36bca6a9193
Samuel Martineau <martineau.samuel.1990@gmail.com>
2016-05-01 22:55:57 +0200

    socket emit action middleware

--- src/common/middleware/remoteAction.js
+++
@@ -1,6 +0,0 @@
-export default socket => store => next => action => {
-  if (action.meta && action.meta.remote) {
-    socket.emit('action', Object.assign({}, action));
-  }
-  return next(action);
-}
