efe7001624f13ec78897039520a0b17c31454e60
Samuel Martineau <martineau.samuel.1990@gmail.com>
2016-07-05 00:55:34 +0200

    clean style

--- bin/initDb.js
+++ bin/initDb.js
@@ -1,20 +1,14 @@
 #!/usr/bin/env node
 
-var r = require('rethinkdb');
-var config = require('../config');
+var r = require('rethinkdb')
+var config = require('../config')
 
-var getConnection = r.connect({host: config.DB.host});
+var getConnection = r.connect({host: config.DB.host})
 
-getConnection
-    .then(function(conn) {
-        return r.dbCreate(config.DB.database)
-            .run(conn)
-            .then(function() {
-                Object.keys(config.DB.tables).forEach(function(tableKey) {
-                    r.db(config.DB.database)
-                        .tableCreate(config.DB.tables[tableKey])
-                        .run(conn);
-                });
-            });
+getConnection.then(function (conn) {
+  return r.dbCreate(config.DB.database).run(conn).then(function () {
+    Object.keys(config.DB.tables).forEach(function (tableKey) {
+      r.db(config.DB.database).tableCreate(config.DB.tables[tableKey]).run(conn)
     })
-    .finally(process.exit);
+  })
+}).finally(process.exit)
