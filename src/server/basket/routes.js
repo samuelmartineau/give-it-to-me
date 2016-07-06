18239ac59bf953bd5fb324a5b0eb0d223febb103
Samuel Martineau <martineau.samuel.1990@gmail.com>
2016-07-06 20:46:35 +0200

    clean style

--- src/server/basket/routes.js
+++ src/server/basket/routes.js
@@ -1,26 +1,22 @@
-import logger from '../utils/logger';
-import config from '../../../config';
-import * as serverConstants from '../../common/constants/server';
-import {addToBasket, removeFromBasket} from './services';
+import logger from '../utils/logger'
+import config from '../../../config'
+import * as serverConstants from '../../common/constants/server'
+import {addToBasket, removeFromBasket} from './services'
 
 export default router => {
-    router.post(serverConstants.ROUTES.BASKET, (req, res) => {
-        return addToBasket(req.body.wineId)
-            .then(message => {
-                res.status(200).json(message);
-            })
-            .catch(error => {
-                res.status(500).json(error);
-            });
-    });
+  router.post(serverConstants.ROUTES.BASKET, (req, res) => {
+    return addToBasket(req.body.wineId).then(message => {
+      res.status(200).json(message)
+    }).catch(error => {
+      res.status(500).json(error)
+    })
+  })
 
-    router.delete(serverConstants.ROUTES.BASKET, (req, res) => {
-        return removeFromBasket(req.body.basketId)
-            .then(message => {
-                res.status(200).json(message);
-            })
-            .catch(error => {
-                res.status(500).json(error);
-            });
-    });
-};
+  router.delete(serverConstants.ROUTES.BASKET, (req, res) => {
+    return removeFromBasket(req.body.basketId).then(message => {
+      res.status(200).json(message)
+    }).catch(error => {
+      res.status(500).json(error)
+    })
+  })
+}
