3d7e097f7b1d68cdd2ab7194cf6d10fbe1b280c1
Samuel Martineau <samuel.martineau@axa.fr>
2017-11-08 17:56:05 +0100

    add type to cellar schema

--- packages/give-it-to-me-client/components/Cellar/CellarSchema.js
+++ packages/give-it-to-me-client/components/Cellar/CellarSchema.js
@@ -1,57 +1,22 @@
+// @flow
 import React from "react";
-import { select } from "d3";
 import ReactFauxDOM from "react-faux-dom";
 
 import { cellar } from "gitm-config";
-const {
-  CELLAR_SCHEMA,
-  BOX_COLOR,
-  BOX_BORDER_SIZE,
-  BOX_BORDER_COLOR,
-  CANVAS_WIDTH,
-  CANVAS_HEIGHT,
-  SELECTED_COLOR
-} = cellar;
-import { drawBottle, drawCellar, drawBottles } from "./utils";
+const { CANVAS_WIDTH, CANVAS_HEIGHT } = cellar;
+import { drawCellar, drawBottles } from "./utils";
 
-const CellarSchema = ({
-  wine,
-  wines = [],
-  bottles = [],
-  selectedCells = {},
-  isBoxClickable = () => {},
-  viewMode,
-  selectMode,
-  onSelectBox = () => {}
-}) => {
+type Props = {
+  bottles: Array
+};
+
+const CellarSchema = ({ bottles = [] }: Props) => {
   let svgContainer = ReactFauxDOM.createElement("svg");
-  let boxId = 0;
   svgContainer.setAttribute("viewBox", `0 0 ${CANVAS_WIDTH} ${CANVAS_HEIGHT}`);
 
   drawCellar(svgContainer);
 
   drawBottles(svgContainer, bottles);
-
-  wines.forEach(wine => {
-    if (wine.bottles) {
-      wine.bottles.forEach(bottle => {
-        drawBottle(
-          svgContainer,
-          WINE_TYPES[wine.wineType].color,
-          bottle.box,
-          bottle.cell,
-          false,
-          viewMode
-        );
-      });
-    }
-  });
-
-  Object.keys(selectedCells).forEach(box => {
-    selectedCells[box].forEach(cell => {
-      drawBottle(svgContainer, SELECTED_COLOR, box, cell, false, viewMode);
-    });
-  });
 
   return <div>{svgContainer.toReact()}</div>;
 };
