6cbc2012bf39f9623e5792e785536bdb6755fa23
Samuel Martineau <martineau.samuel.1990@gmail.com>
2017-12-24 00:48:06 +0100

    feat: empty search

--- packages/give-it-to-me-client/components/MessageManager/MessageManager.stories.js
+++ packages/give-it-to-me-client/components/MessageManager/MessageManager.stories.js
@@ -2,11 +2,20 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import MessageManager from './MessageManager';
 import FavoriteBorder from 'material-ui-icons/FavoriteBorder';
+import Search from 'material-ui-icons/Search';
 
-storiesOf('MessageManager', module).add('Empty favorites', () => (
-  <MessageManager
-    icon={<FavoriteBorder />}
-    title="Aucun favori pour le moment"
-    message="Pour ajouter des favoris il te suffit de faire une recherche et de cliquer sur le petit coeur dans la carte"
-  />
-));
+storiesOf('MessageManager', module)
+  .add('Empty favorites', () => (
+    <MessageManager
+      icon={<FavoriteBorder />}
+      title="Aucun favori pour le moment"
+      message="Pour ajouter des favoris il te suffit de faire une recherche et de cliquer sur le petit coeur dans la carte"
+    />
+  ))
+  .add('Empty search', () => (
+    <MessageManager
+      icon={<Search />}
+      title="Aucun vin trouvé"
+      message="Impossible de trouver un vin. Peut être que tu as mis trop de filtres?"
+    />
+  ));
