4a97f2be0cf8bc65167521cf4d742da0ba1f3bbc
Samuel Martineau <martineau.samuel.1990@gmail.com>
2017-12-24 00:50:59 +0100

    fix: champ story

--- packages/give-it-to-me-client/components/Wine/WineCard.stories.js
+++ packages/give-it-to-me-client/components/Wine/WineCard.stories.js
@@ -1,32 +1,32 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
-import WineCard from "./WineCard";
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import WineCard from './WineCard';
 
-storiesOf("WineCard", module)
-  .add("default", () => (
+storiesOf('WineCard', module)
+  .add('default', () => (
     <WineCard
       wine={{
-        wineType: "RED",
-        name: "Wine test",
+        wineType: 'RED',
+        name: 'Wine test',
         thumbnailFileName:
-          "https://upload.wikimedia.org/wikipedia/en/9/99/MarioSMBW.png",
+          'https://upload.wikimedia.org/wikipedia/en/9/99/MarioSMBW.png',
         blur:
-          "data:image/gif;base64,R0lGODlhAgADAPIAALlkW6RWTmxjjGJZfTJhoTJclwAAAAAAACH5BAAAAAAAIf8LSW1hZ2VNYWdpY2sNZ2FtbWE9MC40NTQ1NQAsAAAAAAIAAwAAAwQIIUOVADs="
+          'data:image/gif;base64,R0lGODlhAgADAPIAALlkW6RWTmxjjGJZfTJhoTJclwAAAAAAACH5BAAAAAAAIf8LSW1hZ2VNYWdpY2sNZ2FtbWE9MC40NTQ1NQAsAAAAAAIAAwAAAwQIIUOVADs='
       }}
     >
       {wine => wine.name}
     </WineCard>
   ))
-  .add("White wine", () => (
+  .add('White wine', () => (
     <WineCard
       wine={{
-        wineType: "WHITE",
-        name: "White Wine test",
+        wineType: 'WHITE',
+        name: 'White Wine test',
         thumbnailFileName:
-          "https://upload.wikimedia.org/wikipedia/en/9/99/MarioSMBW.png",
+          'https://upload.wikimedia.org/wikipedia/en/9/99/MarioSMBW.png',
         blur:
-          "data:image/gif;base64,R0lGODlhAgADAPIAALlkW6RWTmxjjGJZfTJhoTJclwAAAAAAACH5BAAAAAAAIf8LSW1hZ2VNYWdpY2sNZ2FtbWE9MC40NTQ1NQAsAAAAAAIAAwAAAwQIIUOVADs="
+          'data:image/gif;base64,R0lGODlhAgADAPIAALlkW6RWTmxjjGJZfTJhoTJclwAAAAAAACH5BAAAAAAAIf8LSW1hZ2VNYWdpY2sNZ2FtbWE9MC40NTQ1NQAsAAAAAAIAAwAAAwQIIUOVADs='
       }}
     >
       {wine => (
@@ -36,15 +36,21 @@ storiesOf("WineCard", module)
       )}
     </WineCard>
   ))
-  .add("Champagne", () => (
+  .add('Champagne', () => (
     <WineCard
       wine={{
-        wineType: "CHAMPAGNE",
-        name: "Shake that bottle then make it (pop)",
+        wineType: 'CHAMPAGNE',
+        name: 'Shake that bottle then make it (pop)',
         thumbnailFileName:
-          "https://upload.wikimedia.org/wikipedia/en/9/99/MarioSMBW.png",
+          'https://upload.wikimedia.org/wikipedia/en/9/99/MarioSMBW.png',
         blur:
-          "data:image/gif;base64,R0lGODlhAgADAPIAALlkW6RWTmxjjGJZfTJhoTJclwAAAAAAACH5BAAAAAAAIf8LSW1hZ2VNYWdpY2sNZ2FtbWE9MC40NTQ1NQAsAAAAAAIAAwAAAwQIIUOVADs="
+          'data:image/gif;base64,R0lGODlhAgADAPIAALlkW6RWTmxjjGJZfTJhoTJclwAAAAAAACH5BAAAAAAAIf8LSW1hZ2VNYWdpY2sNZ2FtbWE9MC40NTQ1NQAsAAAAAAIAAwAAAwQIIUOVADs='
       }}
-    />
+    >
+      {wine => (
+        <p>
+          Champagne shower <b>{wine.name}</b>
+        </p>
+      )}
+    </WineCard>
   ));
