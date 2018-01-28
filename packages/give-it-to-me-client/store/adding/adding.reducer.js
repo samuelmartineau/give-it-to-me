d806cdf38ad6d24f536fb9267686a5f2d3e6f708
Samuel Martineau <martineau.samuel.1990@gmail.com>
2018-01-28 11:57:06 +0100

    fix: unselect box

--- packages/give-it-to-me-client/store/adding/adding.reducer.js
+++ packages/give-it-to-me-client/store/adding/adding.reducer.js
@@ -1,15 +1,15 @@
 import { combineReducers } from 'redux';
 import { SELECT_BOX, UNSELECT_BOX } from './adding.types';
+import { omit } from 'ramda';
 
 export const selectedBoxesReducer = (state = [], action) => {
   switch (action.type) {
     case SELECT_BOX: {
       return [...state, action.boxId];
     }
-    // case UNSELECT_BOX: {
-    //   const { [action.boxId]: _, ...rest } = state;
-    //   return rest;
-    // }
+    case UNSELECT_BOX: {
+      return state.filter(id => id !== action.boxId);
+    }
     default:
       return state;
   }
@@ -19,7 +19,10 @@ export const selectedCellsReducer = (state = {}, action) => {
     case SELECT_BOX: {
       return { ...state, [action.boxId]: [action.cellId] };
     }
-    // case UNSELECT_BOX: {
+    case UNSELECT_BOX: {
+      return omit([action.boxId], state);
+    }
+    // case UNSELECT_CELL: {
     //   const { [action.boxId]: _, ...rest } = state;
     //   return rest;
     // }
