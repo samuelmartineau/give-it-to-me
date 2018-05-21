80d4a47eff8a31a8d3a9b51348cadd5dc4e9c174
Samuel Martineau <martineau.samuel.1990@gmail.com>
2018-05-21 21:28:49 +0200

    fix dev script

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -4,7 +4,7 @@ echo "Run client"
 npx next &
 
 echo "Run storybook"
-npx start-storybook -p 6006 --static-dir client --config-dir client/.storybook
+npx start-storybook -p 6006 --static-dir client --config-dir client/.storybook &
 
 echo "Run server"
 cd $PROJECT_ROOT'/server'
@@ -12,16 +12,3 @@ npx nodemon index.js
 
 wait
 echo "Client and Server are running"
-
-    # "flow": "flow",
-    # "dev": "npm-run-all -p dev:**",
-    # "dev:next": "next",
-    # "dev:storybook": "start-storybook -p 6006",
-    # "build": "next build",
-    # "start": "next start",
-    # "storybook": "start-storybook -p 6006",
-    # "build-storybook": "build-storybook"
-
-    # "dev": "nodemon index.js",
-    # "start": "node index.js",
-    # "initDb": "bin/initDb.js"
