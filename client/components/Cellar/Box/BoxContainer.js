c9f18c0d0e69173c19af9e207a8c3bc84eeea8d1
Samuel Martineau <martineau.samuel.1990@gmail.com>
2018-12-23 12:18:46 +0100

    fix: svg container height auto

--- client/components/Cellar/Box/BoxContainer.js
+++ client/components/Cellar/Box/BoxContainer.js
@@ -1,6 +1,12 @@
 // @flow
 import React from 'react';
+import styled from 'styled-components';
 import { cellar } from '~/config';
+
+const Svg = styled.svg`
+  width: 100%;
+  height: auto;
+`;
 
 const { CELL_SIZE, CELLAR_SCHEMA, FULL_BOX_WIDTH_CELLS } = cellar;
 
@@ -14,14 +20,13 @@ const BoxContainer = ({ boxId, children }: Props) => {
   const canvasWidth = box.schema[0] * CELL_SIZE;
   const canvasHeigh = box.schema[1] * CELL_SIZE;
   return (
-    <svg
-      style={{ width: '100%', height: '100%' }}
+    <Svg
       width={FULL_BOX_WIDTH_CELLS * CELL_SIZE}
       height={canvasHeigh}
       viewBox={`0 0 ${canvasWidth} ${canvasHeigh}`}
     >
       {children}
-    </svg>
+    </Svg>
   );
 };
 
