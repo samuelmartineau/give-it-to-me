091519243ad0a6302ff8302f98932c9823e37213
Samuel Martineau <martineau.samuel.1990@gmail.com>
2019-01-06 12:35:50 +0100

    register sw

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,11 @@ import theme from '../client/components/Toolkit/theme';
 import ServerSentEventDispatcher from '../client/store/ServerSentEventDispatcher';
 
 class MyApp extends App {
+  componentDidMount() {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/service-worker.js');
+    }
+  }
   render() {
     const { Component, pageProps, store } = this.props;
     return (
