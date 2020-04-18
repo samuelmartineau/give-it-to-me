1460f7b88d1cc2f21cbc6a0d2a027a0f87acf8c7
Samuel Martineau <martineau.samuel.1990@gmail.com>
2020-04-18 23:54:39 +0200

    Delete deprecated Container

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { Provider } from 'react-redux';
-import App, { Container } from 'next/app';
+import App from 'next/app';
 import Head from 'next/head';
 import { reduxPage } from '../client/store';
 import theme from '../client/components/Toolkit/theme';
@@ -11,7 +11,7 @@ class MyApp extends App {
   render() {
     const { Component, pageProps, store } = this.props;
     return (
-      <Container>
+      <>
         <Head>
           <title>Give it to me</title>
         </Head>
@@ -23,7 +23,7 @@ class MyApp extends App {
             </>
           </Provider>
         </ThemeProvider>
-      </Container>
+      </>
     );
   }
 }
