02c995356811d90131d86e0a0760d09599e14034
Samuel Martineau <martineau.samuel.1990@gmail.com>
2020-04-19 17:06:19 +0200

    Type selectors

--- client/store/selectors.ts
+++ client/store/selectors.ts
@@ -5,59 +5,71 @@ import * as search from './search/search.selectors';
 import * as remove from './remove/remove.selectors';
 import * as browse from './browse/browse.selectors';
 import { getBoxCells } from '../components/Cellar/utils';
+import { RootState } from '.';
 
 export const getWineById = (state, wineId) =>
   wines.getWineById(state.wines.map, wineId);
 
-export const getCellsUsedInBox = (state, boxId) =>
+export const getCellsUsedInBox = (state: RootState, boxId: number) =>
   bottles.getCellsUsedInBox(state.bottles.cells, boxId);
-export const getBottleById = (state, boxId) =>
+export const getBottleById = (state: RootState, boxId: number) =>
   bottles.getBottleById(state.bottles.map, boxId);
-export const getBottlesInBox = (state, boxId) =>
+export const getBottlesInBox = (state: RootState, boxId: number) =>
   bottles.getBottlesInBox(state.bottles.cells, boxId);
-export const getBottleByPosition = (state, boxId, cellId) =>
+export const getBottleByPosition = (state: RootState, boxId: number, cellId) =>
   bottles.getBottleByPosition(state.bottles.cells, boxId, cellId);
-export const getSelectedCellsInBox = (state, boxId) =>
+export const getSelectedCellsInBox = (state: RootState, boxId: number) =>
   adding.getSelectedCellsInBox(state.adding.selectedCells, boxId);
-export const isCellSelected = (state, boxId, cellId) =>
-  adding.isCellSelected(state.adding.selectedCells, boxId, cellId);
-export const isBoxSelected = (state, boxId) =>
+export const isCellSelected = (
+  state: RootState,
+  boxId: number,
+  cellId: number
+) => adding.isCellSelected(state.adding.selectedCells, boxId, cellId);
+export const isBoxSelected = (state: RootState, boxId: number) =>
   adding.isBoxSelected(state.adding.selectedBoxes, boxId);
-export const isModelValid = state =>
+export const isModelValid = (state) =>
   adding.isModelValid(state.adding.model, state.adding.selectedBoxes);
-export const getAddModel = state =>
+export const getAddModel = (state: RootState) =>
   adding.getAddModel(state.adding.model, state.adding.selectedCells);
-export const isBoxSelectable = (state, boxId) => {
+export const isBoxSelectable = (state: RootState, boxId: number) => {
   const alreadySelected = isBoxSelected(state, boxId);
   const bottles = getCellsUsedInBox(state, boxId);
   const cells = getBoxCells(boxId);
   return !alreadySelected && cells.length > bottles.length;
 };
-export const isCellSelectable = (state, boxId, cellId) => {
+export const isCellSelectable = (
+  state: RootState,
+  boxId: number,
+  cellId: number
+) => {
   const alreadySelected = isCellSelected(state, boxId, cellId);
   const cellIds = getCellsUsedInBox(state, boxId);
   return !alreadySelected && !cellIds.includes(cellId);
 };
-export const isWineFiltered = (state, wineId) => {
+export const isWineFiltered = (state: RootState, wineId: number) => {
   const wines = getWinesFiltered(state);
   return search.isWineFiltered(state.search, wines, wineId);
 };
 
-export const getWinesFiltered = state =>
+export const getWinesFiltered = (state: RootState) =>
   search.getWinesFiltered(state.wines, state.search);
-export const getFiltersCount = state => search.getFiltersCount(state.search);
+export const getFiltersCount = (state) => search.getFiltersCount(state.search);
 
-export const isWineInBox = (state, boxId, wineId) => {
-  const wineIds = getBottlesInBox(state, boxId).map(bottle => bottle.wine_id);
+export const isWineInBox = (
+  state: RootState,
+  boxId: number,
+  wineId: number
+) => {
+  const wineIds = getBottlesInBox(state, boxId).map((bottle) => bottle.wine_id);
   return wineIds.includes(wineId);
 };
 
-export const getWineBottles = (state, wineId) => {
+export const getWineBottles = (state: RootState, wineId: number) => {
   const { bottleIds } = getWineById(state, wineId);
-  return bottleIds.map(bottleId => getBottleById(state, bottleId));
+  return bottleIds.map((bottleId) => getBottleById(state, bottleId));
 };
 
-export const getWineBottlesAsMap = (state, wineId) => {
+export const getWineBottlesAsMap = (state: RootState, wineId: number) => {
   return getWineBottles(state, wineId).reduce((acc, bottle) => {
     if (!acc[bottle.box]) {
       acc[bottle.box] = {};
@@ -67,21 +79,23 @@ export const getWineBottlesAsMap = (state, wineId) => {
   }, {});
 };
 
-export const getRemovedBottles = state =>
+export const getRemovedBottles = (state: RootState) =>
   remove.getRemovedBottles(state.remove);
-export const isBottleSelectedToBeRemoved = (state, bottleId) =>
-  remove.isBottleSelectedToBeRemoved(state.remove, bottleId);
+export const isBottleSelectedToBeRemoved = (
+  state: RootState,
+  bottleId: number
+) => remove.isBottleSelectedToBeRemoved(state.remove, bottleId);
 
-export const isBoxBrowsed = (state, boxId) =>
+export const isBoxBrowsed = (state: RootState, boxId: number) =>
   browse.isBoxBrowsed(state.browse, boxId);
 
-export const isBoxBrowseable = (state, boxId) => {
+export const isBoxBrowseable = (state: RootState, boxId: number) => {
   const alreadyBrowsed = isBoxBrowsed(state, boxId);
   const bottles = getCellsUsedInBox(state, boxId);
   return !alreadyBrowsed && bottles.length > 0;
 };
 
-export const getBrowsedWine = state => {
+export const getBrowsedWine = (state: RootState) => {
   const { boxId, cellId } = state.browse;
   const bottle = getBottleByPosition(state, boxId, cellId);
   return bottle && getWineById(state, bottle.wine_id);
