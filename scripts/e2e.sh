f6ff92c2d14bf892e07711b6099cbac92f0b3fb9
Samuel Martineau <martineau.samuel.1990@gmail.com>
2020-05-01 18:49:10 +0200

    Add comments

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -5,12 +5,17 @@ export GITM_DB_FILE=$HERE/e2e/fake_db.db
 
 set -e
 
+echo "Clear previous DB"
 rm -f $GITM_DB_FILE
 
+echo "Create empty new DB"
 node scripts/init.js --dbPath=$GITM_DB_FILE
 
+echo "Build the client"
 NODE_ENV=production npm run build
 
+echo "Run the server"
 NODE_ENV=production node index.js &
 
+echo "Expose on entry URL"
 node scripts/reverse-proxy/index.js
