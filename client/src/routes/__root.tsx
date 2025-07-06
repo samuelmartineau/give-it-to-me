b8826b67333674d83d9dba437b7240b7418a8a23
root <root@Dell.>
2025-07-06 11:15:27 +0200

    remove deprecated package

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -2,7 +2,6 @@ import { createRootRoute, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 import React, { useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
-import { HelmetProvider } from 'react-helmet-async';
 import theme from '../components/Toolkit/theme';
 import ServerSentEventDispatcher from '../store/ServerSentEventDispatcher';
 import * as Sentry from '@sentry/react';
@@ -16,14 +15,12 @@ Sentry.init({
 
 export const Route = createRootRoute({
   component: () => (
-    <HelmetProvider>
-      <ThemeProvider theme={theme}>
-        <>
-          <ServerSentEventDispatcher />
-          <Outlet />
-          {import.meta.env.DEV && <TanStackRouterDevtools />}
-        </>
-      </ThemeProvider>
-    </HelmetProvider>
+    <ThemeProvider theme={theme}>
+      <>
+        <ServerSentEventDispatcher />
+        <Outlet />
+        {import.meta.env.DEV && <TanStackRouterDevtools />}
+      </>
+    </ThemeProvider>
   ),
 });
