8d31366f9b42d119b6a751d31fba76e5d25e7604
samuelmartineau <martineau.samuel.1990@gmail.com>
2025-07-06 16:02:46 +0200

    fix github actions

---
+++ e2e/start.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+export GITM_FILE_DIRECTORY=$SCRIPT_DIR/tmp/files
+export GITM_DB_FILE=$SCRIPT_DIR/fake_db.db
+export GITM_SERVER_PORT=4000
+
+export NODE_ENV=production 
+
+echo create E2E folder 
+mkdir -p $SCRIPT_DIR/tmp/files
+
+echo "Clear previous DB"
+rm -f $GITM_DB_FILE
+
+cd "$SCRIPT_DIR/.."
+
+echo "Create empty new DB"
+node ./scripts/init.js --dbPath=$GITM_DB_FILE
+
+echo "Build the client"
+npm run build --prefix client
+concurrently "serve $GITM_FILE_DIRECTORY -p 3005" "node server/server.js" &
