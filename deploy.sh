c150b04aa500ad3ae6b77cec511846af3ffe87e0
samuelmartineau <martineau.samuel.1990@gmail.com>
2025-07-08 22:10:58 +0200

    fix: deploy config

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 sshpass -p $SSH_PASSWORD rsync -avz client/dist/ "$REMOTE_USER@$REMOTE_HOST:$REMOTE_CLIENT_PATH"
 sshpass -p $SSH_PASSWORD rsync -avz server/src/ "$REMOTE_USER@$REMOTE_HOST:$REMOTE_SERVER_PATH/src"
 sshpass -p $SSH_PASSWORD rsync -avz server/package.json "$REMOTE_USER@$REMOTE_HOST:$REMOTE_SERVER_PATH"
+sshpass -p $SSH_PASSWORD rsync -avz config "$REMOTE_USER@$REMOTE_HOST:$REMOTE_SERVER_PATH/.."
 sshpass -p $SSH_PASSWORD ssh "$REMOTE_USER@$REMOTE_HOST:$REMOTE_SERVER_PATH" 'npm i --only=production'
 sshpass -p $SSH_PASSWORD ssh "$REMOTE_USER@$REMOTE_HOST" 'pm2 restart cave'
